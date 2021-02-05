5b58fccd68b86dcafe50daebc1e0b5b1fe517d35
Onur Sercan <57360358+onursercanyilmaz@users.noreply.github.com>
2021-02-05 21:26:27 +0300

    Add files via upload

---
+++ QuizGame/main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for q in question_data:
+     question = Question(q["text"], q["answer"])
+     question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+     quiz.next_question()
+
+print("You have completed the quiz!\n"
+      f"Your final score is: {quiz.score}/{quiz.question_number}")
